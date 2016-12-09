3f0616764c9f7b3bfc76653cadddad276e0a73a1
36bit <36bit@users.noreply.github.com>
2016-12-08 19:30:01 -0800

    Updating itstar to more clearly indicate it is licensed under the GPLv3+. Permission obtained from John Wilson in June 2016 and included here to memorialize it. Lars Brinkhoff has also made changes. Submitting this change to him for inclusion into the repository. He can indicate that he licenses his changes under the same terms by incorporating this commit into the repository.

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
+# This file is part of itstar.
+#
+# itstar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# itstar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with itstar.  If not, see <http://www.gnu.org/licenses/>.
+
 itstar: itstar.o dirlst.o pack.o tapeio.o tm03.o unpack.o zopen.o
 	cc -o itstar itstar.o dirlst.o pack.o tapeio.o tm03.o unpack.o zopen.o
 	strip itstar
--- itstar.c
+++ itstar.c
@@ -10,6 +10,21 @@
   07/15/1998  JMBW  -c, -r, -t functions finished.
   07/18/1998  JMBW  -x function finished.
 
+  This file is part of itstar.
+
+  itstar is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  itstar is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with itstar.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #include <dirent.h>
@@ -473,8 +488,12 @@ static void usage(int rc)
 {
 	fprintf(stderr,"\
 \n\
-ITSTAR V1.10  By John Wilson <wilson@dbit.com>\n\
+ITSTAR V1.10  Copyright (C) By John Wilson <wilson@dbit.com>\n\
 Access ITS DUMP tapes\n\
+\n\
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n\
 \n\
 Usage:  itstar switches file1 file2 file3 ...\n\
 \n\
